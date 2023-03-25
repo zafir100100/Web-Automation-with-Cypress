a7396f8ae446c8e5591ca4c336fb7233f1fc2050
Zafir <55599023+zafir100100@users.noreply.github.com>
2023-03-25 15:00:57 +0600

    for stopping auto loading

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   chromeWebSecurity: false, // for avoiding chrome web security
   e2e: {
+    watchForFileChanges: false, // for avoiding watching for file changes
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
