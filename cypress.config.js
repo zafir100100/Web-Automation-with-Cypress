844a4bee84d7fa199eecab145484334921a0d30e
Zafir <55599023+zafir100100@users.noreply.github.com>
2023-03-25 14:59:18 +0600

    added for bypassing chrome web security

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false, // for avoiding chrome web security
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
