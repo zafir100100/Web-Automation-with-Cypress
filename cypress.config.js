6657103fc4e0d7a76209c57045de79e32b086e81
Zafir <55599023+zafir100100@users.noreply.github.com>
2023-03-25 16:19:33 +0600

    cypress-mochawesome-reporter added

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,22 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   // chromeWebSecurity: false, // for avoiding chrome web security
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     // watchForFileChanges: false, // for avoiding watching for file changes
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+      require('cypress-mochawesome-reporter/plugin')(on);
+
+
+      // set priority of test cases if needed
+      // config.specPattern = [
+      //   'cypress/e2e/Login.cy.js',
+      //   'cypress/e2e/AddToCart.cy.js'
+      // ]
+
+      // return config;
     },
   },
 });
